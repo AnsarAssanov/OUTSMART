335695bf912eed6adbf51acc56ff8610c5ae245e
AnsarAssanov <assanov.pavlodar@gmail.com>
2026-05-28 03:14:19 +0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import Navbar from "./components/Navbar.tsx";
-import Hero from "./components/Hero.tsx";
-import Features from "./components/Features.tsx";
-import Curriculum from "./components/Curriculum.tsx";
-import Showcase from "./components/Showcase.tsx";
-import CallToAction from "./components/CallToAction.tsx";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Curriculum from "./components/Curriculum";
+import Showcase from "./components/Showcase";
+import CallToAction from "./components/CallToAction";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-[#050505]">
-      <Navbar />
-
       <main>
         <Hero />
         <Features />
