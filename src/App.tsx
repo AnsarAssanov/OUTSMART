e96f81fe65241913019741597114158a7837747d
AnsarAssanov <assanov.pavlodar@gmail.com>
2026-05-28 03:07:00 +0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-import { Navbar } from "./components/Navbar";
-import { Hero } from "./components/Hero";
-import { Features } from "./components/Features";
-import { Curriculum } from "./components/Curriculum";
-import { Showcase } from "./components/Showcase";
-import { CallToAction } from "./components/CallToAction";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Curriculum from "./components/Curriculum";
+import Showcase from "./components/Showcase";
+import CallToAction from "./components/CallToAction";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-[#050505]">
       <Navbar />
+
       <main>
         <Hero />
         <Features />
